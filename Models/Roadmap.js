ca23b5d25121471e4f402da6675e132c257d67da
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-05-12 17:26:31 +0800

    push
    push

--- Models/Roadmap.js
+++ Models/Roadmap.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
-const RoadmapSchema = new mongoose.Schema({
-    title: {
-        type: String
+const RoadmapSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String
+        },
+        image: {
+            type: String
+        },
+        description: {
+            type: String
+        }
     },
-    image: {
-        type: String
-    },
-    description: {
-        type: String
+    {
+        timestamps:true
     }
-})
+)
 
 module.exports = mongoose.model("Roadmap", RoadmapSchema)
