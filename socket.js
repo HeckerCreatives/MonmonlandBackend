fb08bc0c55ca453d397632cf103e5c3dd5e174e7
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-07-06 14:38:16 +0800

    asd
    sad

--- socket.js
+++ socket.js
@@ -8,37 +8,22 @@ const User = require('./Models/Users')
 
 
 const socket = io => {
-    let users = []
+    // let users = []
 
     app.use(require('express').static(path.join(__dirname, 'public')));
-
-    io.on('connection', (socket) => {
-        console.log(`⚡: ${socket.id} user just connected!`)
-
-        socket.on("message", data => {
-          io.emit("messageResponse", data)
-        })
     
-        socket.on("typing", data => (
-          socket.emit("typingResponse", data)
-        ))
-    
-        socket.on("privateChat", ({ senderId, recipientId, message, data }) => {
-          // Find the recipient socket by their ID
-          const recipientSocket = io.sockets.sockets.get(recipientId);
-          if (recipientSocket) {
-            // Emit the private message to the recipient
-            recipientSocket.emit("privateChatResponse", {
-              senderId,
-              message,
-              data
-            });
-          } else {
-            // Handle if the recipient is not found (e.g., user not connected)
-            console.log(`Recipient socket not found: ${recipientId}`);
-          }
+    io.on("connection", (socket) => {
+      const users = [];
+      for (let [id] of io.of("/").sockets) {
+        users.push({
+          userID: id,
+          // username: socket.username,
         });
+      }
+      socket.emit("users", users);
 
+      socket.join(socket.id);
+        
         socket.on("private message", ({ content, to }) => {
           const message = {
             content,
@@ -46,26 +31,69 @@ const socket = io => {
             to,
           };
           socket.to(to).to(socket.id).emit("private message", message);
-          // messageStore.saveMessage(message);
+          console.log(message)
         });
-        
-        socket.on("newUser", data => {
-          users.push(data)
-          io.emit("newUserResponse", users)
-        })
 
         socket.on('image message', (data) => {
           // Broadcast the image to all connected clients
           socket.emit('image message', data);          
         });
 
-        socket.on('disconnect', () => {
-          console.log('🔥: A user disconnected');
-          users = users.filter(user => user.socketID !== socket.id)
-          io.emit("newUserResponse", users)
-          socket.disconnect()
-        });
+        // socket.on('disconnect', () => {
+        //   console.log('🔥: A user disconnected');
+        //   users = users.filter(user => user.socketID !== socket.id)
+        //   io.emit("newUserResponse", users)
+        //   socket.disconnect()
+        // });
+      // ...
     });
+
+    
+    // io.on('connection', (socket) => {
+    //     console.log(`⚡: ${socket.id} user just connected!`)
+
+    //     // socket.on("message", data => {
+    //     //   io.emit("messageResponse", data)
+    //     // })
+    
+    //     // socket.on("typing", data => (
+    //     //   socket.emit("typingResponse", data)
+    //     // ))
+    
+    //     // socket.on("privateChat", ({ senderId, recipientId, message, data }) => {
+    //     //   // Find the recipient socket by their ID
+    //     //   const recipientSocket = io.sockets.sockets.get(recipientId);
+    //     //   if (recipientSocket) {
+    //     //     // Emit the private message to the recipient
+    //     //     recipientSocket.emit("privateChatResponse", {
+    //     //       senderId,
+    //     //       message,
+    //     //       data
+    //     //     });
+    //     //   } else {
+    //     //     // Handle if the recipient is not found (e.g., user not connected)
+    //     //     console.log(`Recipient socket not found: ${recipientId}`);
+    //     //   }
+    //     // });
+        
+        
+    //     // socket.on("newUser", data => {
+    //     //   users.push(data)
+    //     //   io.emit("newUserResponse", users)
+    //     // })
+
+    //     // socket.on('image message', (data) => {
+    //     //   // Broadcast the image to all connected clients
+    //     //   socket.emit('image message', data);          
+    //     // });
+
+    //     // socket.on('disconnect', () => {
+    //     //   console.log('🔥: A user disconnected');
+    //     //   users = users.filter(user => user.socketID !== socket.id)
+    //     //   io.emit("newUserResponse", users)
+    //     //   socket.disconnect()
+    //     // });
+    // });
 }
 
 module.exports = socket;
