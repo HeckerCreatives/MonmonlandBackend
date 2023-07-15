8058b4aa5713d68dad148460238fa0b12682caeb
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-07-15 16:45:23 +0800

    aa
    aa

--- socket.js
+++ socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000","https://monmontestwebsite.onrender.com/","https://monmontestwebsite.onrender.com",],
+    origin: ["http://localhost:3000","https://monmontestwebsite.onrender.com/","https://monmontestwebsite.onrender.com", "https://mon-mon-land-dashboard-website.vercel.app/","https://mon-mon-land-dashboard-website.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: [
