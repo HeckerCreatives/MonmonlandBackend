1bf31f0180dcbcf09f29eb3472f04bb25041378f
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-08-11 18:50:54 +0800

    posh
    posh

--- Config/socket.js
+++ Config/socket.js
@@ -1,155 +1,122 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const path = require('path');
-
-
+// const { v4: uuidv4 } = require('uuid'); // Import UUID library
 
 function leaveRoom(userID, chatRoomUsers) {
-  return chatRoomUsers.filter((user) => user.id != userID);
+  return chatRoomUsers.filter((user) => user.id !== userID);
 }
 
 const socket = io => {
-    // let users = []
-    const CHAT_BOT = 'ChatBot';
-    let chatRoom = ''; // E.g. javascript, node,...
-    let allUsers = []; // All users in current chat room
-    let chatRoomUsers ;
-    let queue = {}; // Store queues for each room
+  const CHAT_BOT = 'ChatBot';
+  let chatRoom = '';
+  let adminrooms = [];
+  let allUsers = [];
+  let chatRoomUsers;
+  let queue = {}; // Store queues for each room
 
-    app.use(require('express').static(path.join(__dirname, 'public')));
+  app.use(require('express').static(path.join(__dirname, 'public')));
+
+  io.on("connection", (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
     
-    io.on("connection", (socket) => {
-      console.log(`⚡: ${socket.id} user just connected!`)
-      
-      // Add a user to a room
-      socket.on('join_room', (data) => {
-          const { username, room } = data; // Data sent from client when join_room event emitted
 
-          //  Check if the room already has two users || 
-           const roomUsers = io.sockets.adapter.rooms.get(room);
-           if (roomUsers && (roomUsers.size >= 2 && room !== username)) {
-             // Send an error message to the client indicating that the room is full
-             socket.emit('room_full', {
-               message: `The Admin is still Processing other transaction`,
-             });
-             return;
-           }
+    // io.of("/").adapter.on("create-room", (room) => {
+    //   console.log(`room ${room} was created`);
+    // });
 
-          socket.join(room); // Join the user to a socket room
+    // socket.on('create-room', (room) => {
+    //   console.log(`${room} was created`)
+    //   adminrooms.push(room)
+    //   console.log(allRooms)
+    // })
 
-          // Add this
-          let __createdtime__ = Date.now(); // Current timestamp
-          // Send message to all users currently in the room, apart from the user that just joined
-          socket.to(room).emit('receive_message', {
-            message: `${username} has joined the chat room`,
-            username: CHAT_BOT,
+    // socket.on('allroom', ()=>{
+    //   socket.emit('rooms', io.sockets.adapter.rooms);
+    // })
+
+    // const allRooms = (io.sockets.adapter.rooms);
+    
+
+    socket.on('join_room', (data) => {
+      const { username, room } = data;
+      const roomUsers = io.sockets.adapter.rooms.get(room);
+      let __createdtime__ = Date.now();
+      if (!roomUsers || roomUsers.size < 2) {
+        socket.join(room);
+        
+        socket.to(room).emit('receive_message', {
+          message: `${username} has joined the chat room`,
+          username: CHAT_BOT,
+          __createdtime__,
+        });
+
+        io.to(room).emit('user_joined', { username, room });
+
+        socket.emit('receive_message', {
+          message: `Welcome ${username}`,
+          username: CHAT_BOT,
+          __createdtime__,
+        });
+
+        chatRoom = room;
+        allUsers.push({ id: socket.id, username, room,});
+        chatRoomUsers = allUsers.filter((user) => user.room === room);
+        socket.to(room).emit('chatroom_users', chatRoomUsers);
+        socket.emit('chatroom_users', chatRoomUsers);
+
+      } else {
+        // Queue the user and emit a queue message
+        if (!queue[room]) {
+          queue[room] = [];
+        }
+        queue[room].push({ id: socket.id, username });
+        const queuePosition = queue[room].length;
+        socket.emit('queue_message', {
+          message: `The room is currently full. your queing number is ${queuePosition}.`,
+        });
+      }
+
+      socket.on('send_message', (data) => {
+        const { image, message, username, room, __createdtime__ } = data;
+        io.in(room).emit('receive_message', data);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected from the chat');
+        const user = allUsers.find((user) => user.id === socket.id);
+        if (user?.username) {
+          allUsers = leaveRoom(socket.id, allUsers);
+          socket.to(chatRoom).emit('chatroom_users', allUsers);
+          socket.to(chatRoom).emit('receive_message', {
+            message: `${user.username} has disconnected from the chat.`,
             __createdtime__,
           });
 
-          // Send welcome msg to user that just joined chat only
-          socket.emit('receive_message', {
-            message: `Welcome ${username}`,
-            username: CHAT_BOT,
-            __createdtime__,
-          });
-
-          // Save the new user to the room
-          // chatRoom = room;
-          allUsers.push({ id: socket.id, username, room });
-
-          // Add user to queue
-          // queue[room].push(socket.id);
-
-          // Emit queue position to user
-          // const position = queue[room].length;
-          // socket.emit('queue_position', position);
-
-          // // Check if the user is at the front of the queue
-          // if (queue[room][0] === socket.id) {
-          //   socket.emit('your_turn');
-          // }
-
-          chatRoomUsers = allUsers.filter((user) => user.room === room);
-          socket.to(room).emit('chatroom_users', chatRoomUsers);
-          socket.emit('chatroom_users', chatRoomUsers);
-
-          // // Find the room owner's socket ID
-          // const roomOwnerSocket = chatRoomUsers.find(user => user.username === room);
-          
-          // if (roomOwnerSocket && roomOwnerSocket.id !== socket.id) {
-          //   // Send a notification to the room owner
-          //   io.to(roomOwnerSocket.id).emit('receive_notification', {
-          //     message: `${username} has joined your chat room`             
-          //   });
-          // }
-
-            // Check if the user trying to join is the room owner
-            if (username !== room) {
-              // Find the room owner's socket ID
-              const roomOwnerSocket1 = chatRoomUsers.find(user => user.username === room);
-
-              // If the room owner is not in the room, send an error message to the user
-              if (!roomOwnerSocket1 || !roomUsers.has(roomOwnerSocket1.id)) {
-                socket.emit('room_not_allowed', {
-                  message: `You cannot join this room. The room owner is not present.`,
-                });
-                return;
-              }
-            }
-
-          socket.on('send_message', (data) => {
-            const {image, message, username, room, __createdtime__ } = data;
-            io.in(room).emit('receive_message', data); // Send to all users in room, including sender
-          });
-
-          // socket.on('leave_room', (data) => {
-          //   const { username, room } = data;
-          //   socket.leave(room);
-          //   const __createdtime__ = Date.now();
-          //   // Remove user from memory
-          //   allUsers = leaveRoom(socket.id, allUsers);
-          //   socket.to(room).emit('chatroom_users', allUsers);
-          //   socket.to(room).emit('receive_message', {
-          //     username: CHAT_BOT,
-          //     message: `${username} has left the chat`,
-          //     __createdtime__,
-          //   });
-          //   console.log(`${username} has left the chat`);
-          // });
-
-          socket.on('disconnect', () => {
-            console.log('User disconnected from the chat');
-            const user = allUsers.find((user) => user.id == socket.id);
-            if (user?.username) {
-              allUsers = leaveRoom(socket.id, allUsers);
-              socket.to(chatRoom).emit('chatroom_users', allUsers);
-              socket.to(chatRoom).emit('receive_message', {
-                message: `${user.username} has disconnected from the chat.`,
+          // Handle queue
+          if (queue[chatRoom] && queue[chatRoom].length > 0) {
+            const nextUser = queue[chatRoom].shift();
+            const nextSocket = io.sockets.sockets.get(nextUser.id);
+            if (nextSocket) {
+              nextSocket.join(chatRoom);
+              nextSocket.emit('queue_message', {
+                message: `Now its your turn.`,
+              });
+              allUsers.push({ id: nextUser.id, username: nextUser.username, room: chatRoom });
+              chatRoomUsers = allUsers.filter((user) => user.room === chatRoom);
+              io.in(chatRoom).emit('chatroom_users', chatRoomUsers);
+              nextSocket.to(chatRoom).emit('receive_message', {
+                message: `${nextUser.username} has joined the chat room`,
+                username: CHAT_BOT,
                 __createdtime__,
               });
             }
-          });
-          // // Remove user from the queue
-          // queue[room] = queue[room].filter((id) => id !== socket.id);
-
-          // // Check if the user was at the front of the queue
-          // if (queue[room][0] === socket.id) {
-          //   // Emit event to let the next user know it's their turn
-          //   io.to(queue[room][0]).emit('your_turn');
-          // }
-          });
+          }
+        }
+      });
     });
-
-    
-  
+  });
 }
 
 module.exports = socket;
-
-// app.use(cors())
-
-
-   
-// http.listen(PORT, () => {
-//     console.log(`Server listening on ${PORT}`);
-// });
