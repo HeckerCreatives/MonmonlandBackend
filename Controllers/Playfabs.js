9a74696f72e393248081d16358d69aa21ddd7c1b
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-10-23 22:25:49 +0800

    posh

--- Controllers/Playfabs.js
+++ Controllers/Playfabs.js
@@ -15,71 +15,76 @@ exports.registration = async (req, res) => {
         Password: password,
         Email: email
     };
-    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.Recaptcha_SECRET_KEY}&response=${token}`;
 
-    await fetch(url, {
-        method: 'post',
-        headers: {
-            // "Accept": "application/json",
-            "Content-Type": "application/json"
+    PlayFabClient.RegisterPlayFabUser(playFabUserData, (error, result) => {
+        if(result){
+            PlayFab._internalSettings.sessionTicket = result.data.SessionTicket;
+            PlayFabClient.ExecuteCloudScript({
+                FunctionName: "FinishRegistration",
+                FunctionParameter: {
+                    sponsor: sponsor,
+                    phone: phone,
+                    email: email,
+                    playerUsername: username,
+                },
+                ExecuteCloudScript: true,
+                GeneratePlayStreamEvent: true,
+            }, async (error2, result2) => {
+                if(result2.data.FunctionResult.message !== "success"){
+
+                    PlayFabAdmin.DeleteMasterPlayerAccount({PlayFabId: result.data.PlayFabId}, (error3, result3) => {
+                        if(result3){
+                            res.json({message: "failed", data: "There is a problem in registration of your account please try again"})
+                        } else if(error3) {
+                            res.json({message: "Failed", data: error3.errorMessage})
+                        }
+                    })
+
+                } else if (error2) {
+
+                    PlayFabAdmin.DeleteMasterPlayerAccount({PlayFabId: result.data.PlayFabId}, (error3, result3) => {
+                        if(result3){
+                            res.json({message: "failed", data: "There is a problem in registration of your account please try again"})
+                        } else if(error3) {
+                            res.json({message: "Failed", data: error3.errorMessage})
+                        }
+                    })
+
+                    res.json({message: "failed", data: error2.errorMessage})
+                    
+                } else {
+                    res.json({message: "success"})
+                }
+            })
+
+        } else if (error){
+            res.json({message: "failed", data: error.errorMessage})
         }
     })
-    .then(response => response.json())
-    .then(google_response => {
-        if(google_response.hasOwnProperty("score")){
-            if(google_response.score <= 0.5){
-                return res.json({message: "failed", data: "please verify that you're a human"})
-            } else {
-                PlayFabClient.RegisterPlayFabUser(playFabUserData, (error, result) => {
-                    if(result){
-                        PlayFab._internalSettings.sessionTicket = result.data.SessionTicket;
-                        PlayFabClient.ExecuteCloudScript({
-                            FunctionName: "FinishRegistration",
-                            FunctionParameter: {
-                                sponsor: sponsor,
-                                phone: phone,
-                                email: email,
-                                playerUsername: username,
-                            },
-                            ExecuteCloudScript: true,
-                            GeneratePlayStreamEvent: true,
-                        }, async (error2, result2) => {
-                            if(result2.data.FunctionResult.message !== "success"){
-            
-                                PlayFabAdmin.DeleteMasterPlayerAccount({PlayFabId: result.data.PlayFabId}, (error3, result3) => {
-                                    if(result3){
-                                        res.json({message: "failed", data: "There is a problem in registration of your account please try again"})
-                                    } else if(error3) {
-                                        res.json({message: "Failed", data: error3.errorMessage})
-                                    }
-                                })
-            
-                            } else if (error2) {
-            
-                                PlayFabAdmin.DeleteMasterPlayerAccount({PlayFabId: result.data.PlayFabId}, (error3, result3) => {
-                                    if(result3){
-                                        res.json({message: "failed", data: "There is a problem in registration of your account please try again"})
-                                    } else if(error3) {
-                                        res.json({message: "Failed", data: error3.errorMessage})
-                                    }
-                                })
-            
-                                res.json({message: "failed", data: error2.errorMessage})
-                                
-                            } else {
-                                res.json({message: "success"})
-                            }
-                        })
-            
-                    } else if (error){
-                        res.json({message: "failed", data: error.errorMessage})
-                    }
-                })
-            }
-        } else {
-            return res.json({message: "failed", data: "There's a problem with the server please try again later."})
-        }
-    })
-    .catch(error => res.json({message: "failed", data: error }));
+
+    // const url = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.Recaptcha_SECRET_KEY}&response=${token}`;
+
+    // await fetch(url, {
+    //     method: 'post',
+    //     headers: {
+    //         // "Accept": "application/json",
+    //         "Content-Type": "application/json"
+    //     }
+    // })
+    // .then(response => response.json())
+    // .then(google_response => {
+    //     console.log(google_response)
+    //     console.log(token)
+    //     if(google_response.hasOwnProperty("score")){
+    //         if(google_response.score <= 0.5){
+    //             return res.json({message: "failed", data: "please verify that you're a human"})
+    //         } else {
+                
+    //         }
+    //     } else {
+    //         return res.json({message: "failed", data: "There's a problem with the server please try again later."})
+    //     }
+    // })
+    // .catch(error => res.json({message: "failed", data: error }));
     
 }
