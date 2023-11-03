adba92e1800791ba536d2a7a5a8dff1484119289
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-11-03 22:16:18 +0800

    wew

--- Routes/Nowpayment.js
+++ Routes/Nowpayment.js
@@ -4,5 +4,5 @@ const Nowpayment = require('../Nowpayment/nowpayment');
 
 router.post('/funds', Nowpayment.createinvoicefunds)
 router.post("/bundles", Nowpayment.createinvoicebundles)
-
+router.post("/verify", Nowpayment.verifypayments)
 module.exports = router
