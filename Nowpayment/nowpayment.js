61012b12e2aac9b8f2d501bb5eccd8f2a6869364
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-11-04 19:03:21 +0800

    asdasd

--- Nowpayment/nowpayment.js
+++ Nowpayment/nowpayment.js
@@ -294,7 +294,7 @@ exports.verifypayments = (request, response) => {
             const sorted_request_data = JSON.stringify(body);
             
             const hmac = crypto.createHmac('sha512', process.env.ipnkey)
-            hmac.update(JSON.stringify(request.body, Object.keys(request.body).sort()))
+            hmac.update(JSON.stringify(body))
             const wew = hmac.digest('hex');
 
             console.log("hmac: ", wew)
