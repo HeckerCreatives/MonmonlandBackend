e905f6df7752862f2ab52ac9a856c7ad897a2dda
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-01-16 22:29:23 +0800

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 async function connectToDatabase() {
   const client = new MongoClient(process.env.MONGOOSE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
   await client.connect();
-  return client.db('sampleAPI');
+  return client.db(process.env.databasename);
 }
 
 module.exports = { connectToDatabase };
